5b89faf67fdb97b0809cf960283b10e79bfd4fa1
조용제 <whehdrms6810@naver.com>
2022-12-16 09:53:52 +0900

    [CREATE] introduce.dart

---
+++ lib/screens/bottom/introduce.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Introduce extends StatelessWidget {
+  const Introduce({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: Center(
+        child: Column(
+          children: const [Text("Introduce")],
+        ),
+      ),
+    );
+  }
+}
