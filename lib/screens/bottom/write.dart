1d92b1f69ad2c678fbc05993952afbd7c5cb71b3
조용제 <whehdrms6810@naver.com>
2022-12-16 09:54:07 +0900

    [CREATE] write.dart

---
+++ lib/screens/bottom/write.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Write extends StatelessWidget {
+  const Write({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: Center(
+        child: Column(
+          children: const[
+            Text("Write")
+          ],
+        ),
+      ),
+    );
+  }
+}
