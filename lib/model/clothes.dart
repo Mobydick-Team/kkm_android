bc75441051c6ad4f416dac1a7dbb8a51aefd334e
조용제 <whehdrms6810@naver.com>
2022-12-16 09:52:23 +0900

    [CREATE] clothes.dart

---
+++ lib/model/clothes.dart
@@ -0,0 +1,10 @@
+class ClothesList {
+  String userNmae;
+  int heart;
+  String image;
+  String price;
+  String clothName;
+  ClothesList(
+    this.userNmae, this.heart, this.image, this.price, this.clothName
+  );
+}
