d9eb27df58531b124b5bc644ab41591d4d1f04ac
조용제 <whehdrms6810@naver.com>
2022-12-16 09:51:56 +0900

    [CREATE] common.dart

---
+++ lib/common/common.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CommonColor {
+  //색을 정리해둔 코드
+  static Color buttonNull = const Color(0xFFDFE6FF);
+  static Color blue = const Color(0xFF6573F5);
+}
