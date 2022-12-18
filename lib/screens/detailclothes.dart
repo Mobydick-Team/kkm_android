e382d41deb8274292ffc8d9cd30f99de741030a8
조용제 <whehdrms6810@naver.com>
2022-12-18 13:21:59 +0900

    [UPDATE] width, height

--- lib/screens/detailclothes.dart
+++ lib/screens/detailclothes.dart
@@ -12,123 +12,217 @@ class _DetailClothesState extends State<DetailClothes> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 10.w),
-                    child: IconButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        icon: Icon(
-                          Icons.arrow_back_ios,
-                          color: Colors.black,
-                          size: 27.h,
-                        )),
-                  ),
-                  Text(
-                    "상세정보",
-                    style:
-                        TextStyle(fontSize: 23.sp, fontWeight: FontWeight.w400),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 15.h),
-                child: Container(
-                  width: double.infinity,
-                  height: 400.h,
-                  color: Colors.black,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 7.h),
-                child: Row(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Column(
+          children: [
+            Column(
+              children: [
+                Row(
                   mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Padding(
-                      padding: EdgeInsets.only(left: 8.w, right: 12.w),
+                      padding: EdgeInsets.only(left: 10.w),
+                      child: IconButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          icon: Icon(
+                            Icons.arrow_back_ios,
+                            color: Colors.black,
+                            size: 27.h,
+                          )),
+                    ),
+                    Text(
+                      "상세정보",
+                      style: TextStyle(
+                          fontSize: 22.sp, fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 15.h),
                       child: Container(
-                        width: 35.w,
-                        height: 35.h,
-                        decoration: const BoxDecoration(
-                            shape: BoxShape.circle, color: Colors.black),
+                        width: double.infinity,
+                        height: 400.h,
+                        color: Colors.black,
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsets.only(top: 5.h),
-                      child: Text(
-                        "k99._.e",
-                        style: TextStyle(fontSize: 14.sp, color: Colors.black),
+                      padding: EdgeInsets.only(top: 7.h),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 8.w, right: 12.w),
+                            child: Container(
+                              width: 35.w,
+                              height: 35.h,
+                              decoration: const BoxDecoration(
+                                  shape: BoxShape.circle, color: Colors.black),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 5.h),
+                            child: Text(
+                              "k99._.e",
+                              style: TextStyle(
+                                  fontSize: 14.sp, color: Colors.black),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 14.w, right: 14.w),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "검정 싱글 크롭 자켓",
+                                style: TextStyle(
+                                    fontSize: 20.sp,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(
+                                  bottom: 5.h,
+                                ),
+                                child: Text(
+                                  "3000원",
+                                  style: TextStyle(
+                                      fontSize: 20.sp,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.only(left: 14.w, right: 14.w, bottom: 5.h),
+                      child: Container(
+                          width: double.infinity,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Flexible(
+                                child: RichText(
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 3,
+                                    strutStyle: StrutStyle(fontSize: 16.sp),
+                                    text: TextSpan(
+                                      text:
+                                          '김혜린과 유나은은 바보다. 이것은 절대 부정할 수 없는 사실이다. 그 이유는 조용제가 천재이기 때문이다. 반박시 사형',
+                                      style: TextStyle(
+                                          color: Colors.black.withOpacity(0.6),
+                                          height: 1.4,
+                                          fontSize: 14.sp),
+                                    )),
+                              ),
+                            ],
+                          )),
+                    ),
+                    SizedBox(
+                      height: 15.h,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 14.w,
+                        ),
+                        Text(
+                          "상품 상태",
+                          style: TextStyle(
+                              fontSize: 17.sp,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 8.h,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 14.w, right: 70.w),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "오염 O",
+                            style: TextStyle(
+                                fontSize: 15.sp,
+                                fontWeight: FontWeight.w400,
+                                color: const Color(0xff797979)),
+                          ),
+                          Text(
+                            "변색 X",
+                            style: TextStyle(
+                                fontSize: 15.sp,
+                                fontWeight: FontWeight.w400,
+                                color: const Color(0xff797979)),
+                          ),
+                          Text(
+                            "찢어짐 X",
+                            style: TextStyle(
+                                fontSize: 15.sp,
+                                fontWeight: FontWeight.w400,
+                                color: const Color(0xff797979)),
+                          ),
+                          Text(
+                            "구겨짐 X",
+                            style: TextStyle(
+                                fontSize: 15.sp,
+                                fontWeight: FontWeight.w400,
+                                color: const Color(0xff797979)),
+                          ),
+                        ],
                       ),
                     )
                   ],
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.only(left: 14.w, top: 8.h, right: 14.w),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "검정 싱글 크롭 자켓",
-                          style: TextStyle(
-                              fontSize: 20.sp, fontWeight: FontWeight.w500),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(
-                            bottom: 12.h,
-                          ),
-                          child: Text(
-                            "3000원",
-                            style: TextStyle(
-                                fontSize: 20.sp, fontWeight: FontWeight.w400),
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
+            ),
+            Column(
+              children: [
+                SafeArea(
+                  child: Padding(
+                      padding: EdgeInsets.only(
+                          bottom: MediaQuery.of(context)
+                              .viewInsets
+                              .bottom), //위 상태바 없애기
+                      child: Container(
+                        width: double.infinity,
+                        height: 62.h,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(
+                                color: const Color(0xffBEBEBE), width: 1.w),
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20.r),
+                                topRight: Radius.circular(20.r))),
+                                child: Row(
+                                  children: [],
+                                ),
+                      )),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 14.w, right: 14.w, bottom: 5.h),
-                child: Container(
-                    width: double.infinity,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Flexible(
-                          child: RichText(
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 3,
-                              strutStyle: StrutStyle(fontSize: 16.sp),
-                              text: TextSpan(
-                                text:
-                                    '김혜린과 유나은은 바보다. 이것은 절대 부정할 수 없는 사실이다. 그 이유는 조용제가 천재이기 때문이다. 반박시 사형',
-                                style: TextStyle(
-                                    color: Colors.black.withOpacity(0.6),
-                                    height: 1.4,
-                                    fontSize: 14.sp),
-                              )),
-                        )
-                      ],
-                    )),
-              ),
-              SizedBox(
-                height: 5.h,
-              ),
-              
-            ],
-          ),
+              ],
+            ),
+
+          ],
         ),
       ),
     );
