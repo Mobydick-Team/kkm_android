0acc5ed259fc884eb96385df10c3ca8e3c97d581
조용제 <whehdrms6810@naver.com>
2023-01-10 12:11:54 +0900

    [CREATE] chattingmodel.dart

---
+++ lib/model/chattingmodel.dart
@@ -0,0 +1,10 @@
+class ChattingModel {
+  String name;
+  String location;
+  String time;
+  String lastChatting;
+  String profile;
+  int alarm;
+  String status;
+  ChattingModel(this.name, this.location, this.time, this.lastChatting, this.profile, this.alarm, this.status);
+}
