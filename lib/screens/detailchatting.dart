693069fb6b6cbe2cbca7b387156c160a3bec2620
조용제 <whehdrms6810@naver.com>
2023-01-16 00:17:58 +0900

    [CREATE] detailchatting.dart

---
+++ lib/screens/detailchatting.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class DetailChatting extends StatefulWidget {
+  const DetailChatting({super.key});
+
+  @override
+  State<DetailChatting> createState() => _DetailChattingState();
+}
+
+class _DetailChattingState extends State<DetailChatting> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: Center(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
