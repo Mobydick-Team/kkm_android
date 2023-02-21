276ffde8c6b62de70ff8138925938632d7763cfe
조용제 <whehdrms6810@naver.com>
2023-02-21 23:43:44 +0900

    [ADD] 신고하기 페이지 완성

--- lib/screens/report.dart
+++ lib/screens/report.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-
 class Report extends StatelessWidget {
   const Report({super.key});
 
@@ -11,8 +10,94 @@ class Report extends StatelessWidget {
       appBar: null,
       body: Center(
         child: Column(
-          children: const[
-            Text("Report")
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 6.h, left: 12.w, bottom: 7.h),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                        size: 21.w,
+                        color: Colors.black,
+                      )),
+                  Text(
+                    "신고하기",
+                    style: TextStyle(
+                        fontSize: 22.sp,
+                        fontWeight: FontWeight.w500,
+                        color: const Color(0xffD34646)),
+                  ),
+                ],
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: Padding(
+                padding: EdgeInsets.only(top: 15.h, bottom: 15.h, left: 20.w),
+                child: Text(
+                  "음란, 섹드립",
+                  style: TextStyle(fontSize: 21.sp, color: Colors.black),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 13.h,
+            ),
+            InkWell(
+              onTap: () {},
+              child: Padding(
+                padding: EdgeInsets.only(top: 15.h, bottom: 15.h, left: 20.w),
+                child: Text(
+                  "가스라이팅",
+                  style: TextStyle(fontSize: 21.sp, color: Colors.black),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 13.h,
+            ),
+            InkWell(
+              onTap: () {},
+              child: Padding(
+                padding: EdgeInsets.only(top: 15.h, bottom: 15.h, left: 20.w),
+                child: Text(
+                  "패드립",
+                  style: TextStyle(fontSize: 21.sp, color: Colors.black),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 13.h,
+            ),
+            InkWell(
+              onTap: () {},
+              child: Padding(
+                padding: EdgeInsets.only(top: 15.h, bottom: 15.h, left: 20.w),
+                child: Text(
+                  "남탓",
+                  style: TextStyle(fontSize: 21.sp, color: Colors.black),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 13.h,
+            ),
+            InkWell(
+              onTap: () {},
+              child: Padding(
+                padding: EdgeInsets.only(top: 15.h, bottom: 15.h, left: 20.w),
+                child: Text(
+                  "쌍욕",
+                  style: TextStyle(fontSize: 21.sp, color: Colors.black),
+                ),
+              ),
+            ),
           ],
         ),
       ),
