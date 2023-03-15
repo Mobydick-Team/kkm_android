34899d389406365f13189e4f84f0ad4be9001474
조용제 <whehdrms6810@naver.com>
2023-03-15 11:04:35 +0900

    [UPDATE] stylelish bottom bar

--- lib/screens/bottom/bottom.dart
+++ lib/screens/bottom/bottom.dart
@@ -4,6 +4,7 @@ import 'package:kkm/screens/bottom/chat.dart';
 import 'package:kkm/screens/bottom/home.dart';
 import 'package:kkm/screens/bottom/introduce.dart';
 import 'package:kkm/screens/bottom/write.dart';
+import 'package:stylish_bottom_bar/model/bar_items.dart';
 import 'package:stylish_bottom_bar/stylish_bottom_bar.dart';
 
 class Bottombar extends StatefulWidget {
@@ -35,12 +36,15 @@ class _HomePageState extends State<Bottombar> {
   Widget build(BuildContext context) {
     return DefaultTabController(
         //바텀바 코드
+        
         length: 4,
         child: Scaffold(
           extendBody: true,
           bottomNavigationBar: StylishBottomBar(
             items: [
-              AnimatedBarItems(
+              BottomBarItem(
+                  selectedColor: Colors.black,
+                  backgroundColor: Colors.black,
                   icon: Icon(
                     Icons.home_outlined,
                     size: 23.h,
@@ -49,14 +53,12 @@ class _HomePageState extends State<Bottombar> {
                     Icons.home,
                     size: 23.h,
                   ),
-                  selectedColor: Colors.black,
-                  backgroundColor: Colors.black,
                   title: Text(
                     '홈',
                     style:
                         TextStyle(fontSize: 13.sp, fontWeight: FontWeight.w500),
                   )),
-              AnimatedBarItems(
+              BottomBarItem(
                   icon: Icon(
                     Icons.chat_bubble_outline_outlined,
                     size: 23.h,
@@ -72,7 +74,7 @@ class _HomePageState extends State<Bottombar> {
                     style:
                         TextStyle(fontSize: 13.sp, fontWeight: FontWeight.w500),
                   )),
-              AnimatedBarItems(
+              BottomBarItem(
                   icon: Icon(
                     Icons.add_circle_outline,
                     size: 23.h,
@@ -88,7 +90,7 @@ class _HomePageState extends State<Bottombar> {
                     style:
                         TextStyle(fontSize: 13.sp, fontWeight: FontWeight.w500),
                   )),
-              AnimatedBarItems(
+              BottomBarItem(
                   icon: Icon(
                     Icons.person_outline,
                     size: 23.h,
@@ -105,14 +107,16 @@ class _HomePageState extends State<Bottombar> {
                         TextStyle(fontSize: 13.sp, fontWeight: FontWeight.w500),
                   )),
             ],
-            iconSize: 27.h,
-            iconStyle: IconStyle.Default,
-            hasNotch: true,
-            // fabLocation: StylishBarFabLocation.end,
-            opacity: 0.3,
+            option: AnimatedBarOptions(
+              iconSize: 23.h,
+              barAnimation: BarAnimation.fade,
+              iconStyle: IconStyle.Default,
+              opacity: 0.3,
+            ),
             currentIndex: selected ?? 0,
+            hasNotch: true,
             onTap: (index) {
-              controller.jumpToPage(index!);
+              controller.jumpToPage(index);
               setState(() {
                 selected = index;
               });
