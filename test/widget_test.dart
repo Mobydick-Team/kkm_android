1f67eb382291c0da1ff27a4a831a29c4ff6141b8
조용제 <whehdrms6810@naver.com>
2023-03-15 11:08:52 +0900

    [ADD] import kkm/main.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:kkm_android/main.dart';
+import 'package:kkm/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
