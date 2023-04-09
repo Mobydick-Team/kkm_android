cb3db5ec56d02296eda22464a3bc563aa87e8569
조용제 <>
2023-04-10 08:27:43 +0900

    [ADD] black color

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -4,4 +4,5 @@ class CommonColor {
   //색을 정리해둔 코드
   static Color buttonNull = const Color(0xFFDFE6FF);
   static Color blue = const Color(0xFF6573F5);
+  static Color black = const Color(0xff000000);
 }
