84b4ee2708f9ea55083f4a7ff88ce59f5763d6c8
Lovingcats <whehdrms6810@naver.com>
2023-04-10 09:13:05 +0900

    [ADD] white color

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -5,4 +5,6 @@ class CommonColor {
   static Color buttonNull = const Color(0xFFDFE6FF);
   static Color blue = const Color(0xFF6573F5);
   static Color black = const Color(0xff000000);
+  static Color white = const Color(0xffFFFFFF);
+
 }
