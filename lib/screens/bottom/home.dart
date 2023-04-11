a721f226e24396e82c9ea66cd6ce2ea5332bcea3
Lovingcats <whehdrms6810@naver.com>
2023-04-11 13:53:04 +0900

    [ADD] 아래로 일정 스크롤시 floatingactionbutton생성

--- lib/screens/bottom/home.dart
+++ lib/screens/bottom/home.dart
@@ -19,9 +19,28 @@ class _HomePageState extends State<Home> {
   double _value = 40.0;
   int _index = 0;
 
+  ScrollController scrollController = ScrollController();
+  bool showbtn = false;
+
   @override
   void initState() {
-    // TODO: implement initState
+    scrollController.addListener(() {
+      //scroll listener
+      double showoffset =
+          85.h; //Back to top botton will show on scroll offset 10.0
+
+      if (scrollController.offset > showoffset) {
+        showbtn = true;
+        setState(() {
+          //update state
+        });
+      } else {
+        showbtn = false;
+        setState(() {
+          //update state
+        });
+      }
+    });
     super.initState();
   }
 
@@ -36,6 +55,7 @@ class _HomePageState extends State<Home> {
         appBar: null,
         body: SafeArea(
           child: SingleChildScrollView(
+            controller: scrollController,
             child: Center(
               child: Column(
                 children: [
@@ -266,6 +286,33 @@ class _HomePageState extends State<Home> {
                   const Clothes()
                 ],
               ),
+            ),
+          ),
+        ),
+        floatingActionButton: AnimatedOpacity(
+          duration: const Duration(milliseconds: 0), //show/hide animation
+          opacity: showbtn ? 1.0 : 0.0, //set obacity to 1 on visible, or hide
+          child: Container(
+            height: 55.h,
+            width: 55.w,
+            child: FittedBox(
+              child: FloatingActionButton(
+                  elevation: 10.0,
+                  onPressed: () {
+                    scrollController.animateTo(
+                        //go to top of scroll
+                        0.0,
+                        duration: const Duration(
+                            milliseconds: 450), //duration of scroll
+                        curve: Curves.fastOutSlowIn //scroll type
+                        );
+                  },
+                  child: Icon(
+                    Icons.keyboard_arrow_up,
+                    size: 30.w,
+                    color: Colors.black,
+                  ),
+                  backgroundColor: Colors.white.withOpacity(0.79)),
             ),
           ),
         ),
