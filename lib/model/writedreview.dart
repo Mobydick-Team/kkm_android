003f7823e3fe5401889d2f1044395f273b84ba8e
Lovingcats <whehdrms6810@naver.com>
2023-04-24 01:14:37 +0900

    [CREATE] writedreview.dart

---
+++ lib/model/writedreview.dart
@@ -0,0 +1,10 @@
+class WritedReview {
+  String clothImage;
+  String userImage;
+  String clothName;
+  String userName;
+  String review;
+  bool iskkm;
+  WritedReview(this.clothImage, this.userImage, this.clothName, this.userName,
+      this.review, this.iskkm);
+}
