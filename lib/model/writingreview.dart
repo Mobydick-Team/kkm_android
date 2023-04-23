b98b2b05bab44b7a3ec23f0141f37ed6a126f41d
Lovingcats <whehdrms6810@naver.com>
2023-04-24 01:14:48 +0900

    [CREATE] writingreview.dart

---
+++ lib/model/writingreview.dart
@@ -0,0 +1,9 @@
+class WritingReview {
+  String clothImage;
+  String userImage;
+  String clothName;
+  String userName;
+  int clothprice;
+  WritingReview(this.clothImage, this.userImage, this.clothName, this.userName,
+      this.clothprice);
+}
