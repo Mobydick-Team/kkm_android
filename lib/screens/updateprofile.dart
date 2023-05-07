6dfd92e9e1c1aec2b533b7fdc5ce5ebb30f082b4
Lovingcats <whehdrms6810@naver.com>
2023-05-07 20:07:24 +0900

    [ADD] widgets

--- lib/screens/updateprofile.dart
+++ lib/screens/updateprofile.dart
@@ -27,9 +27,10 @@ class _UpdateProfileState extends State<UpdateProfile> {
         title: Text(
           "내 정보 수정",
           style: TextStyle(
-              fontSize: 22.sp,
-              color: Colors.black,
-              fontWeight: FontWeight.w500),
+            fontSize: 22.sp,
+            color: Colors.black,
+            fontWeight: FontWeight.w500,
+          ),
         ),
         elevation: 0.0,
         leading: Padding(
@@ -46,86 +47,135 @@ class _UpdateProfileState extends State<UpdateProfile> {
         ),
       ),
       body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.only(left: 19.5.w, right: 19.5.w),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      "프로필 사진",
-                      style: TextStyle(
-                          fontSize: 16.sp, color: const Color(0xff8E8E8F)),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 13.h, bottom: 13.h),
-                  child: Stack(
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.only(left: 19.5.w, right: 19.5.w),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Container(
-                        width: 322.w,
-                        height: 322.h,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(6.r),
-                          image: const DecorationImage(
-                              image: AssetImage("images/cat.jpg"),
-                              fit: BoxFit.fill),
-                        ),
-                      ),
-                      Container(
-                        width: 322.w,
-                        height: 322.h,
-                        decoration: BoxDecoration(
-                            color: Colors.black.withOpacity(0.2),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(6.r))),
-                      ),
-                      Container(
-                        width: 322.w,
-                        height: 322.h,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                            image: AssetImage("images/cat.jpg"),
-                            fit: BoxFit.fill,
-                          ),
-                        ),
+                      Text(
+                        "프로필 사진",
+                        style: TextStyle(
+                            fontSize: 16.sp, color: const Color(0xff8E8E8F)),
                       ),
                     ],
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      "내 닉네임",
-                      style: TextStyle(
-                          fontSize: 16.sp, color: const Color(0xff8E8E8F)),
+                  Padding(
+                    padding: EdgeInsets.only(top: 13.h, bottom: 13.h),
+                    child: Stack(
+                      children: [
+                        Container(
+                          width: 322.w,
+                          height: 322.h,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6.r),
+                            image: const DecorationImage(
+                                image: AssetImage("images/cat.jpg"),
+                                fit: BoxFit.fill),
+                          ),
+                        ),
+                        Container(
+                          width: 322.w,
+                          height: 322.h,
+                          decoration: BoxDecoration(
+                              color: Colors.black.withOpacity(0.2),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(6.r))),
+                        ),
+                        Container(
+                          width: 322.w,
+                          height: 322.h,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              image: AssetImage("images/cat.jpg"),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(13.r)),
-                    color: const Color(0xffE9E9EA),
                   ),
-                  margin: EdgeInsets.only(top: 8.h, bottom: 20.h),
-                  child: TextField(
-                    controller: _nameController,
-                    textAlign: TextAlign.start,
-                    textAlignVertical: TextAlignVertical.center,
-                    decoration: InputDecoration(
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "내 닉네임",
+                        style: TextStyle(
+                            fontSize: 16.sp, color: const Color(0xff8E8E8F)),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(13.r)),
+                      color: const Color(0xffE9E9EA),
+                    ),
+                    margin: EdgeInsets.only(top: 8.h, bottom: 20.h),
+                    child: TextField(
+                      controller: _nameController,
+                      textAlign: TextAlign.start,
+                      textAlignVertical: TextAlignVertical.center,
+                      decoration: InputDecoration(
+                          hintText: '',
+                          hintStyle: TextStyle(
+                            fontSize: 14.sp,
+                            color: const Color(0xff8E8E8F),
+                          ),
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.only(
+                              left: 16.w, right: 0, top: 0, bottom: 0),
+                          suffixIcon: _nameController.text == ""
+                              ? IconButton(
+                                  padding: EdgeInsets.zero,
+                                  onPressed: () {},
+                                  icon: const Icon(null))
+                              : IconButton(
+                                  padding: EdgeInsets.zero,
+                                  onPressed: () {
+                                    setState(() {
+                                      _nameController.clear();
+                                      FocusScope.of(context).unfocus();
+                                    });
+                                  },
+                                  icon: const Icon(Icons.cancel_outlined))),
+                      onChanged: (value) {
+                        setState(() {});
+                      },
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "위치",
+                        style: TextStyle(
+                            fontSize: 16.sp, color: const Color(0xff8E8E8F)),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(13.r)),
+                      color: const Color(0xffE9E9EA),
+                    ),
+                    margin: EdgeInsets.only(top: 8.h),
+                    child: TextField(
+                      controller: _addressController,
+                      textAlign: TextAlign.start,
+                      textAlignVertical: TextAlignVertical.center,
+                      decoration: InputDecoration(
                         hintText: '',
                         hintStyle: TextStyle(
                           fontSize: 14.sp,
                           color: const Color(0xff8E8E8F),
                         ),
-                        border: InputBorder.none,
-                        contentPadding: EdgeInsets.only(
-                            left: 16.w, right: 0, top: 0, bottom: 0),
                         suffixIcon: _nameController.text == ""
                             ? IconButton(
                                 padding: EdgeInsets.zero,
@@ -139,62 +189,18 @@ class _UpdateProfileState extends State<UpdateProfile> {
                                     FocusScope.of(context).unfocus();
                                   });
                                 },
-                                icon: const Icon(Icons.cancel_outlined))),
-                    onChanged: (value) {
-                      setState(() {});
-                    },
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      "위치",
-                      style: TextStyle(
-                          fontSize: 16.sp, color: const Color(0xff8E8E8F)),
-                    ),
-                  ],
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(13.r)),
-                    color: const Color(0xffE9E9EA),
-                  ),
-                  margin: EdgeInsets.only(top: 8.h),
-                  child: TextField(
-                    controller: _addressController,
-                    textAlign: TextAlign.start,
-                    textAlignVertical: TextAlignVertical.center,
-                    decoration: InputDecoration(
-                      hintText: '',
-                      hintStyle: TextStyle(
-                        fontSize: 14.sp,
-                        color: const Color(0xff8E8E8F),
+                                icon: const Icon(null)),
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.only(
+                            left: 16.w, right: 0, top: 0, bottom: 0),
                       ),
-                      suffixIcon: _nameController.text == ""
-                          ? IconButton(
-                              padding: EdgeInsets.zero,
-                              onPressed: () {},
-                              icon: const Icon(null))
-                          : IconButton(
-                              padding: EdgeInsets.zero,
-                              onPressed: () {
-                                setState(() {
-                                  _nameController.clear();
-                                  FocusScope.of(context).unfocus();
-                                });
-                              },
-                              icon: const Icon(null)),
-                      border: InputBorder.none,
-                      contentPadding: EdgeInsets.only(
-                          left: 16.w, right: 0, top: 0, bottom: 0),
+                      onChanged: (value) {
+                        setState(() {});
+                      },
                     ),
-                    onChanged: (value) {
-                      setState(() {});
-                    },
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
