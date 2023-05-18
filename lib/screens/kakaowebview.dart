a28eb9a805c0738800494260a90a87a1abfef0bb
Lovingcats <whehdrms6810@naver.com>
2023-05-18 11:02:39 +0900

    [CREATE] kakaowebview.dart

---
+++ lib/screens/kakaowebview.dart
@@ -0,0 +1,149 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:kkm/data/http_client.dart';
+import 'package:kkm/provider/user.dart';
+import 'package:kkm/screens/bottom/bottom.dart';
+import 'package:kkm/screens/name.dart';
+import 'package:provider/provider.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Webviewkakao extends StatefulWidget {
+  const Webviewkakao({Key? key}) : super(key: key);
+
+  @override
+  State<Webviewkakao> createState() => _WebviewState();
+}
+
+class _WebviewState extends State<Webviewkakao> {
+  bool close = false;
+  bool postend = false;
+  String code = "";
+  String url = "https://bgit.bssm.kro.kr/oauth/bsm";
+  String accessToken = "";
+  var userdata;
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  WebViewController? _webViewController;
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      userdata = Provider.of<UserData>(context, listen: false);
+    });
+    _webViewController = WebViewController()
+      ..loadRequest(Uri.parse(
+          'https://kauth.kakao.com/oauth/authorize?client_id=9240bec26b639066d5ac5afdbaeb6bb0&redirect_uri=http://localhost:3000/auth/kakao/callback&response_type=code'))
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+          NavigationDelegate(onNavigationRequest: (NavigationRequest request) {
+        if (request.url.contains("?code=")) {
+          print(request.url);
+          setState(() {
+            code = request.url.split("?code=")[1];
+          });
+          getrequest(userdata, code);
+          return NavigationDecision.prevent;
+        } else if (request.url.contains(url)) {}
+
+        if (request.url.startsWith('https://www.youtube.com/')) {
+          return NavigationDecision.prevent;
+        }
+        return NavigationDecision.navigate;
+      }));
+
+    // _webViewController = WebViewController()
+    //   ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    //   ..setBackgroundColor(const Color(0x00000000))
+    //   ..setNavigationDelegate(
+    //     NavigationDelegate(
+    //       onProgress: (int progress) {
+    //         // Update loading bar.
+    //       },
+    //       onPageStarted: (String url) {},
+    //       onPageFinished: (String url) {},
+    //       onWebResourceError: (WebResourceError error) {},
+
+    //     ),
+    //   )
+    //   ..loadRequest(Uri.parse('https://flutter.dev'));
+  }
+
+  void errormessage(var error) {
+    Fluttertoast.showToast(
+        msg: "카카오톡으로 로그인 실패 $error",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.grey,
+        textColor: Colors.white,
+        fontSize: 16.sp);
+  }
+
+  void successmessage() {
+    Fluttertoast.showToast(
+        msg: "환영합니다!",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.grey,
+        textColor: Colors.white,
+        fontSize: 16.sp);
+  }
+
+  void getrequest(var userdata, String code) async {
+    try {
+      String url = 'http://43.200.90.238:3034/auth/kakao/info?code=$code';
+      var parsingData = await sendGetRequest(url, context);
+      print(parsingData);
+      if (parsingData != null) {
+        if (parsingData['signedUp'] != true) {
+          // ignore: use_build_context_synchronously
+          Navigator.push(
+              context, MaterialPageRoute(builder: (_) => const Name()));
+        } else {
+          userdata.inputId(parsingData['kakaoUserInfoResponse']['id']);
+          // ignore: use_build_context_synchronously
+          Navigator.push(
+              context, MaterialPageRoute(builder: (_) => const Bottombar()));
+          successmessage();
+        }
+      } else {}
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebViewWidget(controller: _webViewController!),
+      // WebView(
+      //   initialUrl:
+      //       'https://kauth.kakao.com/oauth/authorize?client_id=9240bec26b639066d5ac5afdbaeb6bb0&redirect_uri=http://localhost:3000/auth/kakao/callback&response_type=code',
+      //   javascriptMode: JavascriptMode.unrestricted,
+      //   navigationDelegate: (NavigationRequest request) {
+      //     if (request.url.contains("?code=")) {
+      //       setState(() {
+      //         code = request.url.split("?code=")[1];
+      //       });
+
+      //       // do not navigate
+      //       return NavigationDecision.prevent;
+      //     } else if (request.url.contains(url)) {}
+
+      //     return NavigationDecision.navigate;
+      //   },
+      //   onWebViewCreated: (WebViewController webViewController) {
+      //     _controller.complete(webViewController);
+      //   },
+      // ),
+    );
+  }
+}
