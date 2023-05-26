f5b050fd691b704f71fa8387bedf2006591ce510
Lovingcats <whehdrms6810@naver.com>
2023-05-26 22:51:01 +0900

    [ADD] inputUserAddress provider

--- lib/provider/user.dart
+++ lib/provider/user.dart
@@ -5,11 +5,17 @@ class UserData extends ChangeNotifier {
   String _userImage = '';
   String _userId = '';
   String _userName = '';
+  String _useraddress = '';
 
   String get accessToken => _accessToekn;
   String get userName => _userName;
   String get userId => _userId;
   String get userImage => _userImage;
+  String get useraddress => _useraddress;
+
+  void inputUserAddress(String userAddress) {
+    _useraddress = userAddress;
+  }
 
   void inputAccessToken(String accessToken) {
     _accessToekn = accessToken;
