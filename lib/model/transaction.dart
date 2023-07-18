3c9293e78aa713f561c3cbc176bf681daa8b8df4
Lovingcats <whehdrms6810@naver.com>
2023-07-19 00:14:23 +0900

    [ADD] class Transaction

---
+++ lib/model/transaction.dart
@@ -0,0 +1,23 @@
+class Transaction {
+  String userName;
+  bool isheartpressed;
+  String image;
+  String price;
+  String clothName;
+  int deposit;
+  int rentalfee;
+  String location;
+  bool transaction;
+  bool isheart;
+  Transaction(
+      this.userName,
+      this.isheartpressed,
+      this.image,
+      this.price,
+      this.clothName,
+      this.deposit,
+      this.rentalfee,
+      this.location,
+      this.transaction,
+      this.isheart);
+}
