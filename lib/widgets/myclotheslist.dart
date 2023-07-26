3e524309e203d964c9361de4ed8f4386761ca13d
Lovingcats <whehdrms6810@naver.com>
2023-07-27 02:51:05 +0900

    [UPDATE] image list 변경

--- lib/widgets/myclotheslist.dart
+++ lib/widgets/myclotheslist.dart
@@ -23,7 +23,9 @@ class _ClothListState extends State<MyClothesList> {
       clothesList.add(ClothesList(
           "메린",
           false,
-          "https://movie-phinf.pstatic.net/20181213_264/1544692854634ss65r_JPEG/movie_image.jpg",
+          [
+            "https://movie-phinf.pstatic.net/20181213_264/1544692854634ss65r_JPEG/movie_image.jpg"
+          ],
           "2000",
           "메린의 미친 옷",
           1000,
