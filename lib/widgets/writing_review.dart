52d3227465e085abfc31b6d54e645e17a79144e0
Lovingcats <whehdrms6810@naver.com>
2023-07-29 14:49:39 +0900

    [ADD] write new path

--- lib/widgets/writing_review.dart
+++ lib/widgets/writing_review.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kkm/model/writingreview.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:kkm/screens/write.dart';
+import 'package:kkm/screens/bottom/introduce/write.dart';
 
 class WritingReviews extends StatefulWidget {
   const WritingReviews({super.key});
